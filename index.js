2c4df64f6dba6707428ff2fd4da4a3762cee7ba1
+ <+>
2023-03-13 22:45:14 +0300

    Variables Lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution in this file!
+ // 1. companyName is set as Scuber:
+ 
+
+ const companyName = "Scuber" 
+
+ // is defined as a constant
+
+ let mostProfitableNeighborhood = "Chelsea"
+
+ // company co
+ let companyCeo
+
+ companyCeo = "Susan Smith"
